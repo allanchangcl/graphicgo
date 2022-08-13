7611b2bd85fec3c8f6f69e7875bbfc22d0264a2f
Chang CL <chang@cheeloy.com>
2022-08-13 12:36:27 +0800

    add duration profiling

--- bubblesort_test.go
+++ bubblesort_test.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
+	"gitlab.com/changcl/gopkgs/profile"
 )
 
 func TestBubbleSort(t *testing.T) {
 	scores := []int{60, 80, 95, 50, 70, 23}
 	length := len(scores)
+	defer profile.Duration(time.Now(), "BubbleSort")
+
 	myBubbleSort(scores, length)
 	sorted := []int{23, 50, 60, 70, 80, 95}
 
